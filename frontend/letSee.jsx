9852341eb2e36935dce8ae5697033d38b02c3606
your name here <msn@aucegypt.edu>
2020-04-06 23:00:19 -0400

    ph1 finished

--- frontend/letSee.jsx
+++ frontend/letSee.jsx
@@ -1,9 +1,21 @@
-import React from "react";
-import ReactDOM from "react-dom";
+//React
+import React from 'react';
+import ReactDOM from 'react-dom';
+//Components
+import Root from './components/root';
+import configureStore from './store/store';
 
 document.addEventListener("DOMContentLoaded", () => {
+    const store = configureStore();
+    // we don't put the store directly on the window because
+    // it can be confusing when debugging, sometimes giving you access to state
+    // when you shouldn't
+    window.getState = store.getState;
+    window.dispatch = store.dispatch;
+
+
     const root = document.getElementById("root");
-    ReactDOM.render(<h1>Welcome to my Store</h1>, root);
+    ReactDOM.render(<Root store={store} />, root);
 });
 
-
+// window.login = login;
