799d5cb6bbfbf9fdad86e3b5fcb6cb23a52f4ad0
your name here <msn@aucegypt.edu>
2020-04-10 13:27:47 -0400

    after seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,28 +1,16 @@
-require 'faker'
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-# faker
-50.times do
-Product.create([{
-    name: Faker::Commerce.product_name ,
-    description: Faker::Lorem.sentence(1) ,
-    price: Faker::Commerce.price,
-    artist_id: 1
+# require 'faker'
+# 
 
-}])
-end
-# create_table "products", force: :cascade do |t|
-#     t.string "name"
-#     t.string "description"
-#     t.string "photourl"
-#     t.string "string"
-#     t.decimal "price"
-#     t.integer "artist_id"
-#     t.datetime "created_at", null: false
-#     t.datetime "updated_at", null: false
-#   end
+# 50.times do
+# Product.create([{
+#     name: Faker::Commerce.product_name ,
+#     description: Faker::Lorem.sentence(1) ,
+#     price: Faker::Commerce.price,
+#     artist_id: 1
+
+# }])
+# end
+# photo="https://i.etsystatic.com/18585293/r/il/501ac2/2203272268/il_1588xN.2203272268_q46p.jpg"
+
+# UPDATE products SET photourl = ‘2203272268_q46p.jpg’ WHERE id=1;
+
