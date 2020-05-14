b60eb2db538c705e8953c0f75ceadece082edbc5
your name here <msn@aucegypt.edu>
2020-05-14 13:22:56 -0400

    refactor 8

--- frontend/reducers/users_reducer.js
+++ frontend/reducers/users_reducer.js
@@ -9,8 +9,6 @@ const usersReducer = (state = {}, action) => {
             return Object.assign({}, state, { [action.currentUser.id]: action.currentUser });
         // case RECEIVE_REVIEW:
         //     return Object.assign({}, state, { [action.author.id]: action.author });
-        // case RECEIVE_BENCH:
-        //     return Object.assign({}, state, action.authors);
         default:
             return state;
     }
