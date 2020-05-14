6cd74b6f6f6036128942eecb2b9bd82da5c405de
your name here <msn@aucegypt.edu>
2020-05-14 12:44:10 -0400

    refactor 7

--- frontend/reducers/entities_reducer.js
+++ frontend/reducers/entities_reducer.js
@@ -1,12 +1,9 @@
 import { combineReducers } from 'redux';
-
-// import benches from './benches_reducer';
 import users from './users_reducer';
 import products from './products_reducer';
 import cartItems from './cart_items_reducer'
 
 export default combineReducers({
-    // benches,
     users,
     products,
     cartItems
