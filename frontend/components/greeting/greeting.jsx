99101417208262a15f37297e858fd56ab7f8c01d
your name here <msn@aucegypt.edu>
2020-05-14 13:30:03 -0400

    refactor 9

--- frontend/components/greeting/greeting.jsx
+++ frontend/components/greeting/greeting.jsx
@@ -16,10 +16,6 @@ class Greeting extends React.Component {
     return (
     
         <div className="splash">
-          {/* <img className="home" src="https://i.etsystatic.com/6332216/r/il/4e13e2/1517419938/il_fullxfull.1517419938_e4xa.jpg" alt="" /> */}
-
-        {/* <ProductIndexContainer /> */}
-      
         </div>
        
 
@@ -31,18 +27,9 @@ class Greeting extends React.Component {
     let { currentUser } = this.props
     return (
       <div>
-        {/* <div>
-                <img className="contemporary" src="https://toasty-dev.s3-us-west-1.amazonaws.com/contemporary_wall_art.jpg" alt=""/>
-            </div> */}
         <h2 className="welcome">{`Welcome back, ${currentUser.name}!`}</h2>
-
-        
         <span className="sign-in-test">You &#8744;</span>  
         <div className="splash">
-          {/* <img className="home" src="https://i.etsystatic.com/6332216/r/il/4e13e2/1517419938/il_fullxfull.1517419938_e4xa.jpg" alt="" /> */}
-
-          {/* <ProductIndexContainer /> */}
-
         </div>
       </div>
     )
